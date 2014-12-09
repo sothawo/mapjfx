331941a71b501644d2299801dcd36dfaf38df981
P.J.Meisch <pj.meisch@sothawo.com>
2014-12-09 18:51:10 +0100

    application body copied from JavaFX sample

--- src/test/java/com/sothaw/mapjfx/TestApp.java
+++ src/test/java/com/sothaw/mapjfx/TestApp.java
@@ -3,10 +3,39 @@
  */
 package com.sothaw.mapjfx;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Test application.
  *
  * @author P.J. Meisch (pj.meisch@sothawo.com).
  */
-public class TestApp {
+public class TestApp extends Application {
+// -------------------------- OTHER METHODS --------------------------
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+// --------------------------- main() method ---------------------------
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
