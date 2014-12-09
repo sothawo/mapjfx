68d72418b2a6f1d7762a42310880b6501f6c6fb8
P.J.Meisch <pj.meisch@sothawo.com>
2014-12-09 14:13:11 +0100

    first classes

---
+++ src/main/java/com/sothawo/mapjfx/MapView.java
@@ -0,0 +1,12 @@
+/*
+ * (c) 2014 P.J. Meisch (pj.meisch@sothawo.com).
+ */
+package com.sothawo.mapjfx;
+
+/**
+ * TODO: description.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+public class MapView {
+}
---
+++ src/test/java/com/sothaw/mapjfx/TestApp.java
@@ -0,0 +1,12 @@
+/*
+ * (c) 2014 P.J. Meisch (pj.meisch@sothawo.com).
+ */
+package com.sothaw.mapjfx;
+
+/**
+ * TODO: description.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+public class TestApp {
+}
