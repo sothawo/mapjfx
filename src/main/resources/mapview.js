991797414c4aa91459d9169733892cc88ee6d8e4
P.J.Meisch <pj.meisch@sothawo.com>
2017-02-03 21:52:51 +0100

    use OpenLayers 3.20.1, adapted to zoom and animation changes

--- src/main/resources/mapview.js
+++ src/main/resources/mapview.js
@@ -105,13 +105,13 @@ JSMapView.prototype.setCenter = function (lat, lon, animationDuration) {
     var newCenter = cFromWGS84([lon, lat]);
     var oldCenter = _view.getCenter();
     if (oldCenter && animationDuration > 1) {
-        var anim = ol.animation.pan({
-            duration: animationDuration,
-            source: oldCenter
+        _view.animate({
+            center: newCenter,
+            duration: animationDuration
         });
-        _map.beforeRender(anim);
+    } else {
+        _view.setCenter(newCenter);
     }
-    _view.setCenter(newCenter);
 };
 
 /**
@@ -122,15 +122,14 @@ JSMapView.prototype.setCenter = function (lat, lon, animationDuration) {
  */
 JSMapView.prototype.setZoom = function (zoom, animationDuration) {
     if (zoom != _view.getZoom()) {
-        var res = _view.getResolution();
-        if (res && animationDuration > 1) {
-            var anim = ol.animation.zoom({
-                resolution: res,
+        if (animationDuration > 1) {
+            _view.animate({
+                zoom: zoom,
                 duration: animationDuration
             });
-            _map.beforeRender(anim);
+        } else {
+            _view.setZoom(zoom);
         }
-        _view.setZoom(zoom);
     }
 };
 
@@ -147,17 +146,10 @@ JSMapView.prototype.setExtent = function (minLat, minLon, maxLat, maxLon, animat
     // lat/lon reversion
     var extent = eFromWGS84([minLon, minLat, maxLon, maxLat]);
     if (animationDuration > 1) {
-        var animPan = ol.animation.pan({
-            duration: animationDuration,
-            source: _view.getCenter()
-        });
-        var animZoom = ol.animation.zoom({
-            resolution: _view.getResolution(),
-            duration: animationDuration
-        });
-        _map.beforeRender(animPan, animZoom);
+        _view.fit(extent, _map.getSize(), {duration: animationDuration});
+    } else {
+        _view.fit(extent, _map.getSize());
     }
-    _view.fit(extent, _map.getSize());
 };
 
 /**
