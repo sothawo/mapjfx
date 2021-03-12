b5b3712e76b78605c2149b1333b8a89e72b7c63d
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-03-12 10:26:45 +0100

    update copyright comments

--- src/main/java/com/sothawo/mapjfx/XYZParam.java
+++ src/main/java/com/sothawo/mapjfx/XYZParam.java
@@ -1,5 +1,5 @@
 /*
- Copyright 2018-2020 Erik Jähne
+ Copyright 20182021 Erik Jähne
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
